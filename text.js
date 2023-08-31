d33d073e98e620b96db96415f63fe6958954cdc1
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-08-31 18:50:14 +0200

    opdracht 11
    moet nog 5 opdrachten

---
+++ text.js
@@ -0,0 +1,128 @@
+let a = 3
+let b = 8
+let result = a + b
+
+console.log(result)
+a = 80
+b = 14
+result = a + b
+
+console.log (result)
+a = 80
+b = 14
+result = a + b
+
+console.log(result)
+a = 2
+b = 9
+result = a + b
+
+console.log(result)
+a = 6
+b = 4
+result = a + b
+
+console.log(result)
+a = 6
+b = 4
+result = a + b
+// opdracht 2
+let x = 10
+let y = 3
+result = x * y
+console.log(result) 
+x = 80
+y = 14
+result = x * y
+
+console.log (result)
+x = 80
+y = 14
+result = x * y
+
+console.log(result)
+x = 2
+y = 9
+result = x * y
+
+console.log(result)
+x = 6
+y = 4
+result = x * y
+
+console.log(result)
+x = 6
+y = 4
+result = x * y
+// opdracht 3
+let verhoging = 100
+let salaris = 2000
+result =  salaris * 0.05
+console.log(result)
+// opdracht 4
+ x = 4
+ y = 2
+ result = x / y 
+ console.log(result)
+// opdracht 5
+ x = 4
+ y = 0
+ result = x / y
+ console.log(result)
+ //opdracht 6
+ let prijs = 400000
+ let percentage = prijs * 0.025
+console.log(percentage)
+//opdracht 7
+prijs = 100 
+let btw = prijs * 1.21
+console.log(btw)
+//opdracht 8
+let naam = "Mike"
+let achternaam = "van der Veer"
+console.log(naam + achternaam)
+// opdracht 9
+ naam = "Mike"
+ achternaam = " van der Veer"
+console.log(naam + achternaam)
+//opdracht 10
+let aan 
+let uit 
+let waar 
+let onwaar
+let goed
+let fout
+let winnen
+let verliezen
+
+aan = true
+uit = false
+waar = true
+onwaar = false
+goed = true
+fout = false
+winnen = true
+verliezen = false
+
+console.log("aan is: " + aan)
+console.log("uit is: " + uit)
+console.log("waar is:" + waar)
+console.log("onwaar is:" + onwaar)
+console.log("goed is:" + goed)
+console.log("fout is:" + fout)
+console.log("winnen is:" + winnen)
+console.log("verlizen is:" + verliezen)
+//opdracht 11
+console.log(2/2)
+console.log(2%2)
+console.log(3%2)
+console.log(4%2)
+console.log(5%2)
+//opdracht 12
+
+
+
+
+
+
+
